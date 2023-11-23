8635e01457a33d2e6aee68e55c96845fbd82628f
exploitenomah <iksploiting@gmail.com>
2023-11-23 10:19:39 +0100

    fix route ordering

--- routes/users.js
+++ routes/users.js
@@ -9,18 +9,17 @@ const {
   getUserBySession,
   updateUserBySession,
   deleteUserBySession,
-  compareUserIdToParamId,
 } = require("../controllers/user")
 const { protect } = require("../middleware/auth")
-
-router.post("/", signup, sendResponse)
-router.post("/login", login, sendResponse)
-router.get("/logout", logout, sendResponse)
-router.get("/:id", protect, getUserById, sendResponse)
 
 router
   .get("/me", protect, getUserBySession, sendResponse)
   .put("/me", protect, updateUserBySession, sendResponse)
   .delete("/me", protect, deleteUserBySession, sendResponse)
+
+router.post("/", signup, sendResponse)
+router.post("/login", login, sendResponse)
+router.get("/logout", logout, sendResponse)
+router.get("/:id", protect, getUserById, sendResponse)
 
 module.exports = router
