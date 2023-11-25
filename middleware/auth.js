3b40de98a58dc743609fa3908ff164f1198bac1e
exploitenomah <iksploiting@gmail.com>
2023-11-25 12:58:55 +0100

    fix auth error

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,18 +3,17 @@ const CustomError = require("../utils/error")
 const { validateToken } = require("../utils/security")
 
 module.exports.protect = routeTryCatcher(async function (req, _res, next) {
+  const authHeader = req.headers["authorization"]
   let token
-  // if ((req.session && !req.session.access_token) || !req.session) {
-    const authHeader = req.headers["Authorization"]
-    if (authHeader) {
-      const token = authHeader.split("Bearer ")[0]
-      console.log(token, "dafjkfskd")
-    }
-    console.log(authHeader, '13', req.headers)
-  // } else token = req.session.access_token
+  if (authHeader) {
+    token = authHeader.split("Bearer ")[0]
+  }
+  console.log(token, "Line: 11")
+  console.log("Line: 12")
   if (!token) return next(new CustomError("Not allowed!", 403))
   user = await validateToken(token)
   if (!user) return next(new CustomError("Not allowed!", 403))
   req.user = user
+  console.log("Line: 17")
   next()
 })
