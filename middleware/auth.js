113e71299d69fa876d442405349904b300edf0dc
exploitenomah <iksploiting@gmail.com>
2023-11-25 12:52:39 +0100

    remove sessions logic

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,14 +4,14 @@ const { validateToken } = require("../utils/security")
 
 module.exports.protect = routeTryCatcher(async function (req, _res, next) {
   let token
-  if ((req.session && !req.session.access_token) || !req.session) {
+  // if ((req.session && !req.session.access_token) || !req.session) {
     const authHeader = req.headers["Authorization"]
     if (authHeader) {
-      const token = authHeader.split("Bearer ")[1]
+      const token = authHeader.split("Bearer ")[0]
       console.log(token, "dafjkfskd")
     }
-    console.log(authHeader, '13')
-  } else token = req.session.access_token
+    console.log(authHeader, '13', req.headers)
+  // } else token = req.session.access_token
   if (!token) return next(new CustomError("Not allowed!", 403))
   user = await validateToken(token)
   if (!user) return next(new CustomError("Not allowed!", 403))
